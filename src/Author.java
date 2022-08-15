9d164934f96c8307f2234eb9bc56f5b3a115beef
ANDRE <klai1@mail.ru>
2022-08-15 18:38:22 +0400

    DZ все

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private String name;
     private String surname;
@@ -21,6 +23,32 @@ public class Author {
     public Author(String name, String surname) {
         this.name = name;
         this.surname = surname;
+    }
+
+    public String getFullName() {
+        return name + " "+surname;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return name.equals(author.name) && surname.equals(author.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
     }
 }
 
--- src/Book.java
+++ src/Book.java
@@ -1,9 +1,11 @@
+import java.util.Objects;
+
 public class Book {
     private String name;
     private Author author;
     private int yeaOfPublication;
 
-    public Book(String name,int yeaOfPublication,Author author) {
+    public Book(String name, int yeaOfPublication, Author author) {
         this.name = name;
         this.yeaOfPublication = yeaOfPublication;
         this.author = author;
@@ -26,5 +28,26 @@ public class Book {
         return yeaOfPublication;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yeaOfPublication == book.yeaOfPublication && name.equals(book.name) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, author, yeaOfPublication);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", author=" + author +
+                ", yeaOfPublication=" + yeaOfPublication +
+                '}';
+    }
 }
 
